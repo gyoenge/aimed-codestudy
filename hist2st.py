fdf3d77b12a2351d3935f1bd1db6879df0cfbbcc
gyoenge <gyoeng94@gmail.com>
2026-04-15 20:45:54 +0900

    add initial version of Hist2ST code study

---
+++ hist2st.py
@@ -0,0 +1,26 @@
+# Hist2ST source : https://github1s.com/biomed-AI/Hist2ST/blob/main/HIST2ST.py 
+
+import torch 
+import torch.nn as nn
+import pytorch_lightning as pl 
+
+class convmixer_block(nn.Module): 
+    def __init__(self, dim, kernel_size):
+        super().__init__()
+        pass 
+
+    def forward(self, x):
+        pass 
+
+
+class mixer_transformer(nn.Module):
+    pass
+
+
+class ViT(nn.Module):
+    pass 
+
+
+class Hist2ST(pl.LightningModule):
+    pass 
+
